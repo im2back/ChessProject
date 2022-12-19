30e2f10141ae12eefc825c41f71b1e7dad5e8c5e
Jefferson Richards <jefferson.richards@outlook.com>
2022-12-19 17:56:49 -0300

    Printing impossible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.InputMismatchException;
-import java.util.Locale;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -12,37 +11,35 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
-
-
-
-ChessMatch chessMatch = new ChessMatch();
-UI.printBoard(chessMatch.getPieces());
-while (true) {
-	try {
-	UI.clearScreen();
-	UI.printBoard(chessMatch.getPieces());
-	System.out.println();
-	System.out.print("Source: ");
-	ChessPosition source = UI.readChessPosition(sc);
-	
-
-	System.out.println();
-	System.out.print("Target: ");
-	ChessPosition target = UI.readChessPosition(sc);
-
-	ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 	}
-	catch(ChessException e) {
-		System.out.println(e.getMessage());
-		sc.nextLine();
-	}
-		catch(InputMismatchException e) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
-	}
-	}
-
-}
 }
